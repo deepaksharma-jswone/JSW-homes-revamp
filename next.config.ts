4aa3ef6b37cd0be7b57e0d05578ebd5c2c6c0675
Deepak Sharma <deepaksharma@Apples-Laptop.local>
2025-01-15 12:13:43 +0530

    feat(initialisation): setting up repo

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**/*",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
